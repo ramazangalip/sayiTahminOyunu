a479e2d0bafd953df234902e5b4e55d8514925f0
ramazangalip <ramazansaidgalip@gmail.com>
2023-07-22 14:53:15 +0300

    22.07.2023

---
+++ script.js
@@ -0,0 +1,32 @@
+var randomMumber = Math.floor(Math.random()* 9999) + 1000;
+var guessCount = 0;
+var guessLimit = 3;// Sahip Olunan Hak
+
+function checkGuess(){
+    var guess = parseInt(document.getElementById("guessInput").value);
+    var result = document.getElementById("result");
+    if(isNaN(guess)|| guess <1000 || guess>9999 ){
+        result.innerHTML = "Lütfen 1000 ile 9999 arasında bir sayı giriniz.";
+
+    }else{
+        guessCount++;
+        if(guess == randomMumber){
+            result.innerHTML = "Tebrikler! Bildiniz"
+            document.getElementById("correctSound").onplay();
+            guessCount = guessLimit;
+        }else if(guess < randomMumber){
+            result.innerHTML = "Çok Düşük"
+        }else{
+            result.innerHTML = "Cok Yüksek";
+        }
+        if(guessCount === guessLimit){
+            result.innerHTML += "Oyun Bitti Numara şuydu:" +" " +randomMumber + "";
+
+        }else{
+            result.innerHTML += " " + (guessLimit - guessCount) + "Tahmin Hakkın Kaldı";
+            document.getElementById("incorrectSound").onplay();
+        }
+
+    }
+
+    }
